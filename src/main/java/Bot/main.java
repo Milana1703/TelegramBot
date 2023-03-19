b758db57833a357515051f563590d2c7d49ddc85
MilanaZhigmont <zhigmont@list.ru>
2023-03-19 22:42:28 +0500

    вывод в консоль Реализован эхо-функционал для консоли.

--- src/main/java/Bot/main.java
+++ src/main/java/Bot/main.java
@@ -4,6 +4,11 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.Scanner;
+
+/**Запуск и обработка исключений <code>TelegramBotsApi</code>
+ *Реализован эхо-функционал для консоли
+ */
 public class main {
     public static void main(String [] args) {
 
@@ -12,6 +17,12 @@ public class main {
             botsApi.registerBot(new TelegramBot());
         } catch (TelegramApiException exception) {
             exception.printStackTrace();
+        }
+
+        Scanner sc = new Scanner(System.in);
+        BotLogic logic = new BotLogic();
+        while(true){
+            System.out.println(sc.nextLine());
         }
     }
 }
